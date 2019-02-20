865b73038e326c3a79eda4087f130f008ae6c3d3
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2019-02-20 17:16:12 -0500

    oh look another crap commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='toxsux',
+    version='2019.02b2',
+    packages=[''],
+    package_dir={'': 'prod'},
+    url='https://gitlab.com/pypy/toxsux',
+    license='MIT',
+    author='Laurențiu Andronache',
+    author_email='laurentiu.andronache@trailung.ro',
+    description='Crap testing.'
+)
